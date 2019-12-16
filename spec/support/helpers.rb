876fb78a833a3d5a8808c8aca2f4ab5a246eb85c
Joel AZEMAR <joel.azemar@gmail.com>
2019-12-16 10:06:18 +0100

    Add RSpec Helpers

---
+++ spec/support/helpers.rb
@@ -0,0 +1,19 @@
+require 'rspec/rails'
+
+module Annotable
+  module RSpec
+    module ControllerRoutes
+      extend ActiveSupport::Concern
+      included do
+        routes { ::Annotable::Engine.routes }
+      end
+    end
+  end
+end
+
+RSpec.configure do |config|
+  %i[routing controller].each do |type|
+    config.include Annotable::RSpec::ControllerRoutes, type: type 
+  end
+  config.include Annotable::Engine.routes.url_helpers, type: :request
+end

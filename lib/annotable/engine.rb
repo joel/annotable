13e3ed4ef1dd5dd48bf5e31434096175252426a1
Joel AZEMAR <joel.azemar@gmail.com>
2019-12-16 11:14:49 +0100

    Enriched Rspec model template

--- lib/annotable/engine.rb
+++ lib/annotable/engine.rb
@@ -7,5 +7,6 @@ module Annotable
       g.orm :active_record, primary_key_type: :uuid
     end
     config.generators.api_only = true
+    config.generators.templates << File.expand_path('../templates', __dir__)
   end
 end
